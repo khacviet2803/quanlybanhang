9c04d431eae2cb878cc83fb2390e36a16efa564b
xyz <abc>
2018-12-28 05:13:10 +0700

    giao dien dang nhap

---
+++ Project/QuanLyBanhNgot/QuanLyBanhNgot/Flogin.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace QuanLyBanhNgot
+{
+    partial class Flogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtDangnhap = new System.Windows.Forms.Button();
+            this.Btthoat = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Btthoat);
+            this.panel1.Controls.Add(this.BtDangnhap);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 183);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtusername);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(339, 59);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ten Dang Nhap :";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(149, 13);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(169, 22);
+            this.txtusername.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtpass);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(6, 77);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(336, 59);
+            this.panel3.TabIndex = 2;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(149, 13);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(169, 22);
+            this.txtpass.TabIndex = 1;
+            this.txtpass.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mat Khau :";
+            // 
+            // BtDangnhap
+            // 
+            this.BtDangnhap.Location = new System.Drawing.Point(63, 142);
+            this.BtDangnhap.Name = "BtDangnhap";
+            this.BtDangnhap.Size = new System.Drawing.Size(92, 23);
+            this.BtDangnhap.TabIndex = 3;
+            this.BtDangnhap.Text = "DangNhap";
+            this.BtDangnhap.UseVisualStyleBackColor = true;
+            // 
+            // Btthoat
+            // 
+            this.Btthoat.Location = new System.Drawing.Point(194, 142);
+            this.Btthoat.Name = "Btthoat";
+            this.Btthoat.Size = new System.Drawing.Size(92, 23);
+            this.Btthoat.TabIndex = 4;
+            this.Btthoat.Text = "Thoat";
+            this.Btthoat.UseVisualStyleBackColor = true;
+            this.Btthoat.Click += new System.EventHandler(this.Btthoat_Click);
+            // 
+            // Flogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 216);
+            this.Controls.Add(this.panel1);
+            this.Name = "Flogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dang Nhap";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Flogin_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btthoat;
+        private System.Windows.Forms.Button BtDangnhap;
+    }
+}
+
---
+++ Project/QuanLyBanhNgot/QuanLyBanhNgot/Flogin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBanhNgot
+{
+    public partial class Flogin : Form
+    {
+        public Flogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btthoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Flogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có thật sự muốn thoát chương trình?", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
